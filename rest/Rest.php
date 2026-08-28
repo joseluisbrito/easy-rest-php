173ebfcc1e399a596c976da3f3302c9254b0b4bb
José Brito <joseluisbrito@gmail.com>
2026-08-27 23:40:15 -0300

    Se corrige la comparación de los CASE para que los haga contra un int y no contra un string

--- rest/Rest.php
+++ rest/Rest.php
@@ -100,6 +100,7 @@ class Rest {
             return $this->reqBody;
       }
 
+      // Se hace un get sin procesar la token, y queda seteada como propiedad de la clase Rest.
       public function getToken() {
         return $this->token;
       }
@@ -123,31 +124,31 @@ class Rest {
 
       public function responseCode($respCode) {
           switch ($respCode) {
-              case '201':
+              case 201:
                   header("HTTP/1.1 201 Created");
                   break;
-              case '304':
+              case 304:
                   header("HTTP/1.1 304 Not Modified");
                   break;
-              case '400':
+              case 400:
                   header("HTTP/1.1 400 Bad Request");
                   break;
-              case '401':
+              case 401:
                   header("HTTP/1.1 401 Unauthorized");
                   break;
-              case '403':
+              case 403:
                   header("HTTP/1.1 403 Forbidden");
                   break;
-              case '404':
+              case 404:
                   header("HTTP/1.1 404 Not Found");
                   break;
-              case '405':
+              case 405:
                   header("HTTP/1.1 405 Method Not Allowed");
                   break;
-              case '406':
+              case 406:
                   header("HTTP/1.1 406 Not Acceptable");
                   break;
-              case '500':
+              case 500:
                   header("HTTP/1.1 500 Internal Server Error");
                   break;
               default:
